cbf4e1a49e54c72fb2c05105cf4bca1086fb73de
markhorvath <markhorvath7@gmail.com>
2018-03-06 13:52:43 -0500

    lec56 arraylists

---
+++ ListandArrayList/src/com/company/GroceryList.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your list.");
+        for(int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        
+    }
+}
