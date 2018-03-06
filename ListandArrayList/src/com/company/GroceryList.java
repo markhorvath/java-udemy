05cdc5fd7ce40a99b973111efb9d19ebc4f7b928
markhorvath <markhorvath7@gmail.com>
2018-03-06 16:31:38 -0500

    lec57 arraylists pt2

--- ListandArrayList/src/com/company/GroceryList.java
+++ ListandArrayList/src/com/company/GroceryList.java
@@ -24,6 +24,15 @@ public class GroceryList {
     public void removeGroceryItem(int position) {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
-        
+    }
+
+    public String findItem(String searchItem) {
+        //boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if(position >= 0) {
+            return groceryList.get(position);
+        } else System.out.println("Search item not found.");
+
+        return null;
     }
 }
